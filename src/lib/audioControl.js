14ae74fa2410a6985ca4ceb1026df4329686f923
Arvind <aruparekh@gmail.com>
2024-03-14 01:32:15 +0530

    refactor(audio-control): move from classes to functional programming

--- src/lib/audioControl.js
+++ src/lib/audioControl.js
@@ -1,55 +1,57 @@
-import { Howl } from 'howler';
+import { Howl } from "howler";
 
-class AudioControl {
-  constructor() {
-    this.cacheMap = {};
-    this.listenerMap = {};
-  }
+const createAudioControl = () => {
+   const cacheMap = {};
+   const listenerMap = {};
 
-  start(data) {
-    const { id, src, startTime, time, engine } = data;
-    let item;
-    if (this.cacheMap[id]) {
-      item = this.cacheMap[id];
-      item.rate(engine.getPlayRate());
-      item.seek((time - startTime) % item.duration());
-      item.play();
-    } else {
-      item = new Howl({ src: [src], loop: false, autoplay: true });
-      this.cacheMap[id] = item;
-      item.on('load', () => {
-        item.rate(engine.getPlayRate());
-        item.seek((time - startTime) % item.duration());
-      });
-    }
-
-    const timeListener = (data) => {
-      const { time } = data;
-      item.seek(time);
-    };
-    const rateListener = (data) => {
-      const { rate } = data;
-      item.rate(rate);
-    };
-    if (!this.listenerMap[id]) this.listenerMap[id] = {};
-    engine.on('afterSetTime', timeListener);
-    engine.on('afterSetPlayRate', rateListener);
-    this.listenerMap[id].time = timeListener;
-    this.listenerMap[id].rate = rateListener;
-  }
-
-  stop(data) {
-    const { id, engine } = data;
-    if (this.cacheMap[id]) {
-      const item = this.cacheMap[id];
-      item.stop();
-      if (this.listenerMap[id]) {
-        this.listenerMap[id].time && engine.off('afterSetTime', this.listenerMap[id].time);
-        this.listenerMap[id].rate && engine.off('afterSetPlayRate', this.listenerMap[id].rate);
-        delete this.listenerMap[id];
+   const start = (data) => {
+      const { id, src, startTime, time, engine } = data;
+      let item;
+      if (cacheMap[id]) {
+         item = cacheMap[id];
+         item.rate(engine.getPlayRate());
+         item.seek((time - startTime) % item.duration());
+         item.play();
+      } else {
+         item = new Howl({ src: [src], loop: false, autoplay: true });
+         cacheMap[id] = item;
+         item.on("load", () => {
+            item.rate(engine.getPlayRate());
+            item.seek((time - startTime) % item.duration());
+         });
       }
-    }
-  }
-}
 
-export default new AudioControl();
+      const timeListener = (data) => {
+         const { time } = data;
+         item.seek(time);
+      };
+      const rateListener = (data) => {
+         const { rate } = data;
+         item.rate(rate);
+      };
+      if (!listenerMap[id]) listenerMap[id] = {};
+      engine.on("afterSetTime", timeListener);
+      engine.on("afterSetPlayRate", rateListener);
+      listenerMap[id].time = timeListener;
+      listenerMap[id].rate = rateListener;
+   };
+
+   const stop = (data) => {
+      const { id, engine } = data;
+      if (cacheMap[id]) {
+         const item = cacheMap[id];
+         item.stop();
+         if (listenerMap[id]) {
+            listenerMap[id].time &&
+               engine.off("afterSetTime", listenerMap[id].time);
+            listenerMap[id].rate &&
+               engine.off("afterSetPlayRate", listenerMap[id].rate);
+            delete listenerMap[id];
+         }
+      }
+   };
+
+   return { start, stop };
+};
+
+export default createAudioControl();
