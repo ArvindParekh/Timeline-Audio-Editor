ca390871b13d97686405315d4a5e83211ad95637
Arvind <aruparekh@gmail.com>
2024-03-13 21:30:53 +0530

    fix: audio looping

--- src/audioControl.js
+++ src/audioControl.js
@@ -15,7 +15,7 @@ class AudioControl {
       item.seek((time - startTime) % item.duration());
       item.play();
     } else {
-      item = new Howl({ src: [src], loop: true, autoplay: true });
+      item = new Howl({ src: [src], loop: false, autoplay: true });
       this.cacheMap[id] = item;
       console.log(item);
       item.on('load', () => {
